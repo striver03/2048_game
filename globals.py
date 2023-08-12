125ee2c3a5a74eabd09af11ee58a214625f4c9a1
Rishabh Prasad <rishabh.prasad2003@gmail.com>
2023-08-12 13:41:55 +0530

    Some optimizations

--- globals.py
+++ globals.py
@@ -1,3 +1,10 @@
-def initialize():
-    global score
+from collections import deque
+
+def initialize(len = 4):
+    global score, listWithZeroes, moves_stack
     score = 0
+    moves_stack = deque()
+    listWithZeroes = []
+    for i in range(len):
+        for j in range(len):
+            listWithZeroes.append([i,j])
